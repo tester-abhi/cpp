0dc10ea98141fbd0094c643e5b7142b0b4779469
tester_abhi <tester.abhi.101@gmail.com>
2024-04-05 00:28:07 +0530

    update: add range validation on input row count

--- print_patterns/simplePyramid.cpp
+++ print_patterns/simplePyramid.cpp
@@ -5,8 +5,17 @@ using namespace std;
 int main(){
 	// cout << "Hello World!" << endl;
 	int n;
-	cout << "enter the number of rows:" << endl;
-	cin >> n; 
+	while(true){
+		cout << "enter the number of rows:" << endl;
+		cin >> n; 
+		if (!(n>0 && n<=10)){
+			cout << "Invalid input, Please enter a positive interger value <= 10" << endl;
+			continue;
+		}else{
+			break;
+		}
+	}
+	
 	for(int i = 0; i < n; i++){
 		for (int j = i+1; j > 0; j--){
 			cout << "*";

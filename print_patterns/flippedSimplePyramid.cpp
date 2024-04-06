3445c56739569cb329e7c28b9aeccaa4c82073e3
tester_abhi <tester.abhi.101@gmail.com>
2024-04-06 23:54:48 +0530

    update: add range validation on input row in flipped pyramid exercise

--- print_patterns/flippedSimplePyramid.cpp
+++ print_patterns/flippedSimplePyramid.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main(){
-    int n = 5;
+    int n;
+    cout << "Enter the number of rows:" << endl;
+    cin >> n; 
+    // int n = 5;
     for(int i = 0; i < n; i++){
         for(int j = n; j >= 0; j--){
             if(j<=i)

23951ec4b7dc8a4560b6be1de23233c086e9dda1
tester_abhi <tester.abhi.101@gmail.com>
2024-04-09 23:57:55 +0530

    update: replace space character with @ character in flipped pyramid exercise

--- strings/stringBasics01.cpp
+++ strings/stringBasics01.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
+#include<string.h>
 
 using namespace std;
 
 int main(){
-    char name[] = {'A', 'B', 'H', 'I', '\0'};
+    // char name[] = {'A', 'B', 'H', 'I', '\0'};
+    string name = "Abhi";
     cout <<  name << endl;
+    cout << "length of name is " << name.length() << endl;
+    cout << "first character of name is " << name[0] << endl;
+    if(name[0] == 'A'){
+        cout << "working" << endl;
+    }
     return 0;
 }
